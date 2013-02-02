787a7cf1d3bc4c18790c54ca13132f46dba4a048
Robert Baker <rjabaker@outlook.com>
2013-02-02 11:26:40 -0600

    Added code to read wikipedia page links

--- public/js/walk.js
+++ public/js/walk.js
@@ -1,15 +1,16 @@
-(function($){
 function iterate(title, state) {
-  var page = buildPage(title);
+  var pageData = readPageData(title); 
+  var node = buildNode(title);
   var nextState = getNextState(page, state);
 
   if(nextState.shouldRender)
   {
-    renderPage(page);
+    saveNode(node);
   }
   if(nextState.shouldContinue)
   {
     var pageRead = readPage(nextState);
+    // Add nodes
     //for(int i = 0; i < pageRead.Length; i++) {
       //iterate(pageRead[i], nextState);
     //}
@@ -23,20 +24,3 @@ function buildPage(title) {
 function getNextState(page, state) {
 
 }
-
-$(document).ready( function() {
-// Assign handlers immediately after making the request,
-// and remember the jqxhr object for this request
-var jqxhr = $.getJSON("http://en.wikipedia.org/w/api.php?action=query&prop=links&format=json&plnamespace=0&pllimit=400&iwurl=&titles=dog&callback=?", function() {
-  console.log("success");
-})
-.success(function() { console.log("second success"); })
-.error(function() { console.log("error"); })
-.complete(function() { console.log("complete"); });
- 
-// perform other work here ...
- 
-// Set another completion function for the request above
-jqxhr.complete(function(){ console.log("second complete"); });
-});
-})(jQuery);
--- public/js/wiki-talk.js
+++ public/js/wiki-talk.js
@@ -1,12 +1,32 @@
-(function($){
-  $(document).ready( function() {
-    var jqxhr = $.getJSON("http://en.wikipedia.org/w/api.php?action=query&prop=links&format=json&plnamespace=0&pllimit=400&iwurl=&titles=dog&callback=?", function() {
-      console.log("success");
-  })
-  .success(function() { console.log("second success"); })
-  .error(function() { console.log("error"); })
-  .complete(function() { console.log("complete"); });
+// (function($){
+//   $(document).ready( function() {
+//     var jqxhr = $.getJSON("http://en.wikipedia.org/w/api.php?action=query&prop=links&format=json&plnamespace=0&pllimit=400&iwurl=&indexpageids=&titles=dog&callback=?", function(data) {
+//       console.log("success");
+//   })
+//   .success(function(data) { console.log("second success"); console.log(data.query.pages[data.query.pageids[0]].links[0]);})
+//   .error(function() { console.log("error"); })
+//   .complete(function() { console.log("complete"); });
    
-  jqxhr.complete(function(){ console.log("second complete"); });
-  });
-})(jQuery);
+//   jqxhr.complete(function(){ console.log("second complete"); });
+//   console.log(jqxhr);
+//   });
+// })(jQuery);
+
+function readPageData(title) {
+  var url = "http://en.wikipedia.org/w/api.php?action=query&prop=links&" +
+    "format=json&plnamespace=0&pllimit=400&iwurl=&indexpageids=&callback=?&titles=" + 
+    title;
+  var json = $.getJSON(url, function() {
+      console.log("Success reading " + title);
+    })
+  .success(function(data) {
+    console.log("Second success reading " + title);
+    alert("Success!");
+    return data;
+  })
+  .error(function(err) {
+    console.log("Error");
+    alert("Error!");
+    return null;   
+  })
+}
